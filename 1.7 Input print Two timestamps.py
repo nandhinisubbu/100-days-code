a29252233deb1403ad858a7a99628a75283c5d1a
nandhinisubbu <42835327+nandhinisubbu@users.noreply.github.com>
2019-08-07 20:34:22 +0530

    Create 1.7 Input print Two timestamps.py

---
+++ 1.7 Input print Two timestamps.py
@@ -0,0 +1,9 @@
+h1 = int(input())
+m1 = int(input())
+s1 = int(input())
+h2 = int(input())
+m2 = int(input())
+s2 = int(input())
+time1 = m1*60 + h1*3600 + s1
+time2 = m2*60 + h2*3600 + s2
+print(time2-time1)
