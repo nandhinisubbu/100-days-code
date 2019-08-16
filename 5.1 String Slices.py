3d8de9acdc73eb1a4a4d263f070337a38df49e96
nandhinisubbu <42835327+nandhinisubbu@users.noreply.github.com>
2019-08-15 21:07:08 -0700

    Create 5.1 String Slices.py

---
+++ 5.1 String Slices.py
@@ -0,0 +1,37 @@
+You are given a string.
+
+1.In the first line, print the third character of this string.
+2.In the second line, print the second to last character of this string.
+3.In the third line, print the first five characters of this string.
+4.In the fourth line, print all but the last two characters of this string.
+5.In the fifth line, print all the characters of this string with even indices (remember indexing starts at 0, so the characters are displayed starting with the first).
+6.In the sixth line, print all the characters of this string with odd indices (i.e. starting with the second character in the string).
+7.In the seventh line, print all the characters of the string in reverse order.
+8.In the eighth line, print every second character of the string in reverse order, starting from the last one.
+9.In the ninth line, print the length of the given string.
+
+
+Example input
+Abrakadabra
+
+Example output
+r
+r
+Abrak
+Abrakadab
+Arkdba
+baaar
+arbadakarbA
+abdkrA
+11
+
+s = input()
+print(s[2])
+print(s[-2])
+print(s[0:5])
+print(s[0:-2])
+print(s[0::2])
+print(s[1::2])
+print(s[::-1])
+print(s[::-2])
+print(len(s))
