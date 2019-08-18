943d3c852fa82fd150b5928f3593f8474404506d
nandhinisubbu <42835327+nandhinisubbu@users.noreply.github.com>
2019-08-18 18:01:45 +0530

    Create 7.3 Lists Greater than left neighbour.py

---
+++ 7.3 Lists Greater than left neighbour.py
@@ -0,0 +1,14 @@
+Statement
+Given a list of numbers, find and print all its elements that are greater than their left neighbor.
+
+Example input
+1 5 2 4 3
+
+Example output
+5 4
+
+a = [int(s) for s in input().split()]
+# Print a value:
+for i in range(1, len(a)):
+    if a[i] > a[i-1]:
+        print(a[i], end=' ')
